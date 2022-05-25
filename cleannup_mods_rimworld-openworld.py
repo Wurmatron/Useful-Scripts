a6f87011f6ba3a3d63c98c2d353b2ef790455f2a
Wurmatron <wurmatron@gmail.com>
2022-05-25 15:13:28 -0400

    Added RimWorld Mod Cleaner for OpenWorld Server

---
+++ cleannup_mods_rimworld-openworld.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+
+
+def cleanup_all_mods(folder):
+    file_names = os.listdir(folder)
+    for file_name in file_names:
+        if os.path.isdir(folder + "/" + file_name):
+            remove_extras_from_mod(folder + "/" + file_name)
+        else:
+            print("Not deleting file '" + folder + "/" + file_name + "'")
+
+
+def remove_extras_from_mod(folder):
+    file_names = os.listdir(folder)
+    for file_name in file_names:
+        if file_name != "About":
+            try:
+                to_be_deleted = folder + "/" + file_name
+                if os.path.isdir(to_be_deleted):
+                    shutil.rmtree(to_be_deleted)
+                else:
+                    os.remove(to_be_deleted)
+                print("Removing -> " + to_be_deleted)
+            except Exception as ex:
+                print('Failed to delete ' + file_name)
+                print(ex)
+
+
+cleanup_all_mods("./mods")
